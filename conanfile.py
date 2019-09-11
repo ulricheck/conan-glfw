2dcc48b59c7241e34ce1ee08a9a20ecfbff8dfbc
Ulrich Eck <ulrich.eck@tum.de>
2019-09-11 20:56:51 +0200

    bumped version to 3.3.0

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ import glob
 
 class GlfwConan(ConanFile):
     name = "glfw"
-    version = "3.2.1"
+    version = "3.3.0"
     description = "The GLFW library - Builds on Windows, Linux and Macos/OSX"
     sources_folder = "sources"
     generators = "cmake"
